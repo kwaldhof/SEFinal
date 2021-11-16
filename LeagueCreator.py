7e0e44268d721bfe0d16a9cc35a3772207a2a8b7
kwaldhof <striker8kaw@gmail.com>
2021-11-16 17:11:23 -0500

    Changed Names of UI

--- LeagueCreator.py
+++ LeagueCreator.py
@@ -11,7 +11,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_MainWindow(object):
+class Ui_CreateLeague(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
@@ -107,7 +107,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
+    ui = Ui_CreateLeague()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
--- MainPage.py
+++ MainPage.py
@@ -11,7 +11,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_L(object):
+class Ui_Leagues(object):
 
     def x(self):
         print("XXXX")
@@ -68,7 +68,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     L = QtWidgets.QMainWindow()
-    ui = Ui_L()
+    ui = Ui_Leagues()
     ui.setupUi(L)
     L.show()
     sys.exit(app.exec_())
