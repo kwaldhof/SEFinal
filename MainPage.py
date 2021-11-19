91a16f88185c6155a55d05ced87d8c6283d95079
kwaldhof <striker8kaw@gmail.com>
2021-11-18 19:27:37 -0500

    idek

--- MainPage.py
+++ MainPage.py
@@ -12,6 +12,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+
 from LeagueCreator import Ui_CreateLeague
 from LeagueFinder import Ui_LeagueFinder
 
