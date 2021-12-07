dc2f9c27568c00b9353d8510958d2ed517172e20
biubiubiupiu21 <94020092+biubiubiupiu21@users.noreply.github.com>
2021-12-07 17:13:27 -0500

    Done

--- Tim_testfile.py
+++ Tim_testfile.py
@@ -1,20 +1,4 @@
 import pandas as pd
 from openpyxl import load_workbook
 
-def writedata():
-    # Initialize xlsx writer
-    writer = pd.ExcelWriter('Tim_Test_data.xlsx', engine='xlsxwriter')
-    workbook = writer.book
-
-    df1 = pd.DataFrame({"A": [1,2,3],
-                        "B": [1,2,3]})
-
-    df1.to_excel(writer,
-                sheet_name="sheet1",
-                startrow=1,
-                startcol=1)
-
-    writer.save()
-
-writedata()
 
