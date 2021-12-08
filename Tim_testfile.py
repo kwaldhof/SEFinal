3b6943aa5115e6993e5888e6d9d10864167ee428
biubiubiupiu21 <94020092+biubiubiupiu21@users.noreply.github.com>
2021-12-07 19:24:42 -0500

    Done

--- Tim_testfile.py
+++ Tim_testfile.py
@@ -6,8 +6,8 @@ import numpy as np
 fname = "Tim_Test_data.xlsx"
 
 data = pd.read_excel(fname,"Sheet2") #read the excel file
-
-new_data = [[1,"WAC",3,3]] #Fetching new record from the frontend (right now I am just set up a record)
+print(data)
+new_data = [[2,"WAC",3,3]] #Fetching new record from the frontend (right now I am just set up a record)
 df2 = pd.DataFrame(new_data, columns=['Team_ID',"Team_Name", "League_ID", "Player_Count"])# Convert this new record to dataframe
 data = data.append(df2,ignore_index=True)#append to the data we got from the excel
 print(data)
@@ -19,6 +19,6 @@ writer.sheets = dict((ws.title, ws) for ws in book.worksheets)#trying to set up 
 
 
 
-data.to_excel(writer,sheet_name="Sheet2",startcol=0,startrow=0)#push the data to the excel
+data.to_excel(writer,sheet_name="Sheet2",index=False)#push the data to the excel
 
 writer.save()
